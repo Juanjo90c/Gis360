4e68a882c9c5413d9435f9fbc945d82589387a78
unknown <juanjo90c@gmail..com>
2023-11-30 19:00:22 -0300

    Initial commit to master

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import logo from './Opcion sin patitas_.png';
+import React, { useState, useEffect } from 'react';
+import './home.css';
+
+
+function Home() {
+  return (
+      <div className="App">
+              <img src={logo} className="App-logo" alt="logo" />
+      </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/nosotros.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect }  from 'react';
+import Lucas from './lucas.png';
+import Tania from './tania.png';
+import Lucia from './lucia.png';
+import Juanjo from './juanjo.png';
+import './nosotros.css';
+
+function Nosotros() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading the line first
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 500); // Adjust the delay time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+      <div className = "nosotrosdiv">
+        <div className = "fototexto">
+          <div className={`line2 ${loaded ? 'loaded' : ''}`}></div>
+          <div className = "fotos">
+            <img src={Lucas} className="foto"/>
+            <img src={Tania} className="foto"/>
+            <img src={Lucia} className="foto"/>
+            <img src={Juanjo} className="foto"/>
+          </div>
+            <div className = "textos">
+              <div className='descripcion'>
+                <h3>LEONCZYK, LUCAS</h3>
+                <p>
+                  Arquitecto<br></br>
+                  Urbanista<br></br>
+                  Especialista en SIG<br></br>
+                </p>
+              </div>
+              <div className='descripcion'>
+                <h3>MALDONADO, TANIA</h3>
+                <p>
+                  Arquitecta<br></br>
+                  Urbanista<br></br>
+                  Especialista en SIG<br></br>
+                </p>
+              </div>
+              <div className='descripcion'>
+                <h3>MISIASZEK, LUCIA</h3>
+                <p>
+                  Arquitecta<br></br>
+                  Urbanista<br></br>
+                  Especialista en SIG<br></br>
+                </p>
+              </div>
+              <div className='descripcion'>
+                <h3>COCHIA, JUAN JOSE</h3>
+                <p>
+                  Software Developer<br></br>
+                </p>
+              </div>  
+          </div>
+        </div>
+        <div className = "aboutus">
+          <h2>SOBRE NOSOTROS</h2>
+          <p>
+            Somos un equipo de trabajo con experiencia de
+            abordaje en múltiples campos disciplinarios, 
+            ofrecemos soluciones integrales a través de la construcción 
+            y análisis de datos, la automatización de
+            procesos y la identificación de necesidades y
+            demandas específicas de cada usuario, empresa
+            u organización con el fin de diseñar respuestas a
+            medida.
+            Uno de nuestros principales servicios es la capacitación de 
+            equipos de trabajo para el manejo de
+            los SIG, ya que entendemos que la informacion
+            debe ser una fuente de datos publica e independiente.
+          </p>
+        </div>
+      </div>
+  );
+}
+
+export default Nosotros;
