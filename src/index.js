b71bc2e2b6b4bd9a51099d203ef58a9f0ab99ac7
unknown <juanjo90c@gmail..com>
2023-12-01 14:37:40 -0300

    e

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ root.render(
       <Route path="contacto" element={<Contacto />} />
     </Route>
   </Routes>
-</HashRouter>
+</HashRouter> 
 );
 
 // If you want to start measuring performance in your app, pass a function
